0fcafb4700bcfabda7f757637218ad79e3684021
vadim.vinnik <vadim.vinnik@gmail.com>
2014-11-03 19:12:22 +0000

    bubble source code

--- sort_test.cpp
+++ sort_test.cpp
@@ -1,11 +1,13 @@
 #include <assert.h>
+#include "sort_test.h"
 #include "insertion.h"
 #include "selection.h"
-#include "sort_test.h"
+#include "bubble.h"
 
 int main() {
   assert(sort_test(insertion_sort<int>));
   assert(sort_test(selection_sort<int>));
+  assert(sort_test(bubble_sort<int>));
 
   return 0;
 }
