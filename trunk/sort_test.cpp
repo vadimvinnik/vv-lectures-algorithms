953a122fe0cc19017b42d5709e2d5ae63d5e8a6b
vadim.vinnik@gmail.com <vadim.vinnik@gmail.com@f80beac5-dc18-e6da-28df-9cb6e296352e>
2014-11-03 10:42:13 +0000

    Source code for insertion

--- trunk/sort_test.cpp
+++ trunk/sort_test.cpp
@@ -1,8 +1,10 @@
 #include <assert.h>
+#include "insertion.h"
 #include "selection.h"
 #include "sort_test.h"
 
 int main() {
+  assert(sort_test(insertion_sort<int>));
   assert(sort_test(selection_sort<int>));
 
   return 0;
